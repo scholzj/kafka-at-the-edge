5f2c2c4892c1f85be1e520498c85b879a55f3f84
Jakub Scholz <www@scholzj.com>
2023-06-04 22:17:47 +0200

    Add metrics
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- hq/frontend/src/main/java/cz/scholz/SensorDataEndpoint.java
+++ hq/frontend/src/main/java/cz/scholz/SensorDataEndpoint.java
@@ -1,6 +1,8 @@
 package cz.scholz;
 
 import cz.scholz.model.SensorData;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import io.quarkus.logging.Log;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
@@ -18,6 +20,7 @@ import java.time.Duration;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Path("/api/sensors")
@@ -28,8 +31,14 @@ public class SensorDataEndpoint {
     @Inject
     KafkaConsumer<String, SensorData> consumer;
 
+    private final MeterRegistry registry;
+
     final Map<String, SensorData> sensorData = new HashMap<>();
     volatile boolean done = false;
+
+    public SensorDataEndpoint(MeterRegistry registry) {
+        this.registry = registry;
+    }
 
     public void initialize(@Observes StartupEvent ev) {
         consumer.subscribe(Collections.singleton(sensorDataTopic));
@@ -38,7 +47,7 @@ public class SensorDataEndpoint {
                 final ConsumerRecords<String, SensorData> consumerRecords = consumer.poll(Duration.ofSeconds(1));
 
                 consumerRecords.forEach(record -> {
-                    sensorData.put(record.key(), record.value());
+                    handleNewSensorData(record.key(), record.value());
                     Log.debugf("Polled Record:(%s, %s, %d, %d)\n",
                             record.key(), record.value(),
                             record.partition(), record.offset());
@@ -46,6 +55,17 @@ public class SensorDataEndpoint {
             }
             consumer.close();
         }).start();
+    }
+
+    private void handleNewSensorData(String key, SensorData value)  {
+        // Add the data to the map queried by the REST API
+        sensorData.put(key, value);
+
+        // Add the record as a Prometheus metric
+        List<Tag> tags = List.of(Tag.of("long", String.valueOf(value.getLongitude())), Tag.of("lat", String.valueOf(value.getLatitude())));
+        registry.gauge("sensor.data.temperature", tags, value.getTemperature());
+        registry.gauge("sensor.data.humidity", tags, value.getHumidity());
+        registry.gauge("sensor.data.pressure", tags, value.getPressure());
     }
 
     public void terminate(@Observes ShutdownEvent ev) {
